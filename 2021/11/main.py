8f0808f8b1bf0d87e8c591c00f9c2102b98affa1
Metin San <metinisan@gmail.com>
2021-12-12 12:09:55 +0100

    Renamed variables.

--- 2021/11/main.py
+++ 2021/11/main.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass, field
-from typing import List
+from dataclasses import dataclass
+from typing import List, Optional
 
 import numpy as np
 
@@ -7,7 +7,7 @@ import numpy as np
 MAX_VALUE = 9
 
 
-def read_grid(filename: str) -> np.ndarray:
+def read_octopus_grid(filename: str) -> np.ndarray:
     grid: List[List[int]] = []
     with open(filename, "r") as file:
         for line in file:
@@ -18,15 +18,15 @@ def read_grid(filename: str) -> np.ndarray:
 
 
 @dataclass
-class Simulator:
-    grid: np.ndarray
+class OctopusFlashSimulator:
+    octopus_grid: np.ndarray
     step: int = 0
     total_flashes: int = 0
-    simultaneous_flash_steps: List[int] = field(default_factory=list)
+    simultaneous_flash_step: Optional[int] = None
 
     def simulate_step(self) -> None:
         self.step += 1
-        grid = self.grid.copy() + 1
+        grid = self.octopus_grid.copy() + 1
         padded_grid = np.pad(grid, pad_width=(1, 1))
 
         flashes = np.argwhere(grid > 9).tolist()
@@ -34,30 +34,34 @@ class Simulator:
 
         while flashes:
             new_flashes = []
-            for i, j in flashes:
-                sliding_window_view = padded_grid[i : i + 3, j : j + 3]
-                extra_flashes = (sliding_window_view <= MAX_VALUE) == (
+
+            for row, col in flashes:
+                sliding_window_view = padded_grid[row : row + 3, col : col + 3]
+                additional_flashes = (sliding_window_view <= MAX_VALUE) == (
                     sliding_window_view + 1 > MAX_VALUE
                 )
-                if indicies := np.argwhere(extra_flashes).tolist():
-                    for ii, jj in indicies:
-                        new_flashes.append([ii - 1 + i, jj - 1 + j])
-                        self.total_flashes += 1
-                padded_grid[i : i + 3, j : j + 3] += 1
+
+                additional_flash_indices = np.argwhere(additional_flashes).tolist()
+                for extra_row, extra_col in additional_flash_indices:
+                    new_flashes.append([extra_row - 1 + row, extra_col - 1 + col])
+                    self.total_flashes += 1
+
+                padded_grid[row : row + 3, col : col + 3] += 1
             flashes = new_flashes
 
         padded_grid[padded_grid > MAX_VALUE] = 0
-        self.grid = padded_grid[1:-1, 1:-1]
-        if (self.grid == 0).all():
-            self.simultaneous_flash_steps.append(self.step)
+        self.octopus_grid = padded_grid[1:-1, 1:-1]
+
+        if self.simultaneous_flash_step is None and (self.octopus_grid == 0).all():
+            self.simultaneous_flash_step = self.step
 
 
 if __name__ == "__main__":
     INPUT_FILE = "input.txt"
-    grid = read_grid(INPUT_FILE)
+    grid = read_octopus_grid(INPUT_FILE)
 
     print("Part 1:")
-    simulator_part1 = Simulator(grid)
+    simulator_part1 = OctopusFlashSimulator(grid)
     for i in range(100):
         simulator_part1.simulate_step()
     print(
@@ -65,9 +69,11 @@ if __name__ == "__main__":
     )
 
     print("\nPart 2:")
-    simulator_part2 = Simulator(grid)
+    simulator_part2 = OctopusFlashSimulator(grid)
     while True:
         simulator_part2.simulate_step()
-        if simulator_part2.simultaneous_flash_steps:
+        if simulator_part2.simultaneous_flash_step is not None:
             break
-    print(f"The first step during which all octopuses flashes is {simulator_part2.step}")
+    print(
+        f"The first step during which all octopuses flashes is {simulator_part2.step}"
+    )
