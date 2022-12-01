822fdba00f23a84172be3434543f53fe02c3fcb3
Metin San <metinisan@gmail.com>
2022-12-01 20:28:05 +0100

    Refactor day 1

--- 2022/day01/src/a.rs
+++
@@ -1,8 +0,0 @@
-use crate::common;
-
-pub fn solution() {
-    let input = common::read_input("input.txt");
-    let sum_of_chunks = common::sum_chunks(&input);
-    let max_chunk = sum_of_chunks.iter().max().unwrap();
-    println!("The most calories carried by an elf is: {}", max_chunk)
-}
--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,8 +1,35 @@
-mod a;
-mod b;
-mod common;
+use std::fs;
 
 fn main() {
-    a::solution();
-    b::solution();
+    let data = read_input("input.txt");
+    let calories = get_summed_elf_calories(&data);
+
+    // Part a)
+    let max_calories = calories.iter().max().unwrap();
+    println!("The most calories carried by an elf is: {}", max_calories);
+
+    // Part b)
+    let top_three_max_calories: u32 = calories.iter().rev().take(3).sum();
+    println!(
+        "The top three elves are carring {} calories",
+        top_three_max_calories
+    );
+}
+
+fn get_summed_elf_calories(input: &str) -> Vec<u32> {
+    let mut sum: u32 = 0;
+    let mut sums = Vec::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            sums.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<u32>().unwrap();
+        }
+    }
+    sums
+}
+
+fn read_input(filename: &str) -> String {
+    fs::read_to_string(filename).expect("Something went wrong reading the file")
 }
