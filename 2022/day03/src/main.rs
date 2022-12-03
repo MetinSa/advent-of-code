65d4e4caaffe1cd3e7417e275d81d096805e1b23
Metin San <metinisan@gmail.com>
2022-12-04 00:30:51 +0100

    Refactor

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -8,21 +8,21 @@ fn main() {
     let alphabet = String::from_utf8((b'a'..=b'z').chain(b'A'..=b'Z').collect()).unwrap();
 
     // Part 1
-    let mut priority: u32 = 0;
+    let mut priority_sum: u32 = 0;
     for line in data.iter() {
         let (first, second) = line.split_at(line.len() / 2);
         let unique_chars: HashSet<char> = first.chars().collect();
         for c in second.chars() {
             if unique_chars.contains(&c) {
-                priority += alphabet.find(c).unwrap() as u32 + 1;
+                priority_sum += alphabet.find(c).unwrap() as u32 + 1;
                 break;
             }
         }
     }
-    println!("Part 1: the sum of the priorities are {}", priority);
+    println!("Part 1: the sum of the priorities are {}", priority_sum);
 
     // Part 2
-    let mut priority: u32 = 0;
+    let mut priority_sum: u32 = 0;
     let mut shared_chars_in_first_two: Vec<char> = Vec::new();
 
     for (first, second, third) in data.iter().tuples() {
@@ -37,19 +37,19 @@ fn main() {
 
         for c in third.chars() {
             if unique_shared_chars_in_first_two.contains(&c) {
-                priority += alphabet.find(c).unwrap() as u32 + 1;
+                priority_sum += alphabet.find(c).unwrap() as u32 + 1;
                 break;
             }
         }
         shared_chars_in_first_two.clear();
     }
-    println!("Part 2: the sum of the priorities are {}", priority);
+    println!("Part 2: the sum of the priorities are {}", priority_sum);
 }
 
 fn read_input(file_path: &str) -> Vec<String> {
     fs::read_to_string(file_path)
         .expect("Failed to read input")
         .split("\n")
-        .map(|s| s.to_string()) // Convert &str to String
+        .map(|s| s.to_string())
         .collect()
 }
