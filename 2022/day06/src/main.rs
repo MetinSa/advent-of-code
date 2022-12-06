fa9003f6959506855a1d438e5c93dcd1fa4a73eb
Metin San <metinisan@gmail.com>
2022-12-06 09:32:08 +0100

    Refactor

--- 2022/day06/src/main.rs
+++ 2022/day06/src/main.rs
@@ -2,28 +2,28 @@ use itertools::Itertools;
 use std::fs;
 
 fn main() {
-    let input_data = fs::read_to_string("input.txt").expect("Unable to read file");
+    let input_string = fs::read_to_string("input.txt").expect("Unable to read file");
+    let signal: Vec<char> = input_string.chars().collect();
 
     // Part 1)
-    let marker_idx = get_marker_idx(&input_data, 4).expect("Marker was not found in part 1");
+    let marker_idx = get_marker_idx(&signal, 4).expect("Marker was not found in part 1");
     println!(
-        "Part 1: start-of-packet marker found after {} chars",
+        "Part 1: start-of-packet marker found after {} characters",
         marker_idx
     );
 
     // Part 2)
-    let marker_idx = get_marker_idx(&input_data, 14).expect("Marker was not found in part 2");
+    let marker_idx = get_marker_idx(&signal, 14).expect("Marker was not found in part 2");
     println!(
-        "Part 2: start-of-message marker found after {} chars",
+        "Part 2: start-of-message marker found after {} characters",
         marker_idx
     );
 }
 
-fn get_marker_idx(data: &str, distinct_chars: usize) -> Option<usize> {
-    let char_collection: Vec<char> = data.chars().collect();
-    for (idx, c) in char_collection.windows(distinct_chars).enumerate() {
-        if c.iter().unique().count() == distinct_chars {
-            return Some(idx + distinct_chars);
+fn get_marker_idx(signal: &Vec<char>, distinct_characters: usize) -> Option<usize> {
+    for (idx, c) in signal.windows(distinct_characters).enumerate() {
+        if c.iter().unique().count() == distinct_characters {
+            return Some(idx + distinct_characters);
         }
     }
     None
