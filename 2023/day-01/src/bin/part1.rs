0241c94473e23c3374873a66c7801df936697714
Metin San <metinisan@gmail.com>
2023-12-02 09:22:30 +0100

    Finished part 2

--- 2023/day-01/src/bin/part1.rs
+++ 2023/day-01/src/bin/part1.rs
@@ -1,20 +1,34 @@
 fn main() {
     let input = include_str!("./input1.txt");
-    let output = part1(input);
+    let output = process(input);
     dbg!(output);
 }
 
-fn part1(input: &str) -> String {
-    "todo!()".to_string()
- }
+fn process(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut iterator = line.chars().filter_map(|character| character.to_digit(10));
+            let first = iterator.next().unwrap();
+            let last = match iterator.last() {
+                Some(number) => number,
+                None => first,
+            };
+            format!("{first}{last}").parse::<u32>().unwrap()
+        })
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "todo!()".to_string());
+    fn test_process() {
+        let input = "1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet";
+        assert_eq!(process(input), 142);
     }
 }
--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,20 +1,55 @@
 fn main() {
     let input = include_str!("./input1.txt");
-    let output = part1(input);
+    let output = process(input);
     dbg!(output);
 }
 
-fn part2(input: &str) -> String {
-    "todo!()".to_string()
- }
+fn process(input: &str) -> u32 {
+    let number_map = vec![
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "t3e"),
+        ("four", "f4r"),
+        ("five", "f5e"),
+        ("six", "s6x"),
+        ("seven", "s7n"),
+        ("eight", "e8t"),
+        ("nine", "n9e"),
+    ];
+    input
+        .lines()
+        .map(|line| {
+            let mut line = line.to_string();
+            for (number, replacement) in number_map.iter() {
+                line = line.replace(number, replacement);
+            }
+            line
+        })
+        .map(|line| {
+            let mut iterator = line.chars().filter_map(|character| character.to_digit(10));
+            let first = iterator.next().unwrap();
+            let last = match iterator.last() {
+                Some(number) => number,
+                None => first,
+            };
+            format!("{first}{last}").parse::<u32>().unwrap()
+        })
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part2("");
-        assert_eq!(result, "todo!()".to_string());
+    fn test_process() {
+        let input = "two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen";
+        assert_eq!(process(input), 281);
     }
 }
